768e93398b706a6e3f19c836b12e99154ce80e02
prathamg108 <pratham.gandhi20@pccoepune.org>
2023-03-11 21:03:14 +0530

    Login Page Created

--- social_network/src/page/login/Login.jsx
+++ social_network/src/page/login/Login.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
-
+import React from "react";
+import "./Login.css";
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="login">
+      <div className="login_main_box">
+        <div className="login_box">
+          <div className="box">
+            <label>Email :</label>
+            <input type="email" id="email" name="email" />
+          </div>
+          <div className="box">
+            <label>Password :</label>
+            <input type="password" id="password" name="password" />
+          </div>
+          <div className="box">
+            <button className="submit_button">Submit</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
