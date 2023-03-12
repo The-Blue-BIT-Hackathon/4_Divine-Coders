7216669abc37c35e7c7839b3bd4e2805337ad11a
Arnav Deshmukh <arnav.deshmukh20@pccoepune.org>
2023-03-12 15:44:10 +0530

    data fetched

--- social_network/src/page/home/Home.jsx
+++ social_network/src/page/home/Home.jsx
@@ -1,40 +1,45 @@
-import React, { useEffect,useState } from 'react'
-import { db } from '../../firebase/Firebase';
-import { getDocs,collection } from 'firebase/firestore';
+import React, { useEffect, useState } from "react";
+import { db } from "../../firebase/Firebase";
+import { getDocs, collection } from "firebase/firestore";
 const Home = () => {
+  const [loading, setLoading] = useState(false);
+  const postCollectionRef = collection(db, "posts");
+  useEffect(() => {
+    console.log("In Home Screen");
+    getPostList();
+  }, []);
+  const [posts, setPosts] = useState([]);
+  const [post, setPost] = useState([]);
 
-
-  const [posts,getPosts]=useState([]);
-  const postCollectionRef=collection(db,"posts");
-  useEffect(()=>{
-getPostList()
-  },[])
-
-
-  const getPostList=async()=>{
-    try{
-
-      const data=await getDocs(postCollectionRef);
-      console.log(data)
-      const filteredData=data.docs.map((doc)=>({
+  const getPostList = async () => {
+    try {
+      const data = await getDocs(postCollectionRef);
+      setLoading(true);
+      console.log(data);
+      const filteredData = data.docs.map((doc) => ({
         ...doc.data(),
-        id:doc.id
+        id: doc.id,
       }));
-      console.log(filteredData)
-      getPosts(filteredData);
-      console.log(posts)
-    }
-    catch(err)
-    {
-      console.error(err)
-    }
+      console.log(filteredData);
 
-  }
-  return (
-    <div>
-        
-    </div>
-  )
-}
+      setPosts(filteredData);
+      setLoading(false);
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
-export default Home
+  const results = [];
+  posts.forEach((post, index) => {
+    results.push(
+      <div key={index}>
+        <h1>{post.useremail}</h1>
+        <h1>{post.post}</h1>
+      </div>
+    );
+  });
+  console.log(typeof posts);
+  return <div>{loading ? <div>Loading</div> : <div></div>}</div>;
+};
+
+export default Home;
