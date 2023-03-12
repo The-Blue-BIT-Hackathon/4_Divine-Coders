6d9ebe905c078863ce4b2674ef151346975a153d
prathamg108 <pratham.gandhi20@pccoepune.org>
2023-03-12 13:28:28 +0530

    addpost created

--- social_network/src/App.jsx
+++ social_network/src/App.jsx
@@ -1,10 +1,10 @@
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 import Login from "./page/login/Login";
 import Signup from "./page/signup/Signup";
 import Navbar from "./Components/navbar/Navbar";
 import Home from './page/home/Home'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Addpost from "./page/addPost/Addpost";
 
 function App() {
   return (
@@ -13,6 +13,7 @@ function App() {
       <Navbar/>
         <Routes>
         <Route exact path="/" element={<Home/>}></Route>
+          <Route exact path="/addpost" element={<Addpost/>}></Route>
           <Route exact path="/login" element={<Login/>}></Route>
           <Route exact path="/signup" element={<Signup />}></Route>
         </Routes>
--- social_network/src/Components/navbar/Navbar.jsx
+++ social_network/src/Components/navbar/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./Navbar.css";
+import checkLogedIn from "../../hook/checkLogin";
+import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <>
@@ -10,10 +12,31 @@ const Navbar = () => {
               <li className="nav-box active">Network</li>
             </div>
             <div className="navbar-space">
-              <li className="nav-box">Add Post</li>
-              <li className="nav-box">Profile</li>
-              <li className="nav-box">Sign Up</li>
-              <li className="nav-box">Login</li>
+              <li className="nav-box">
+                {" "}
+                <Link className="nav-name" to="/addpost">Add Post</Link> 
+              </li>
+              <li className="nav-box">
+                {" "}
+                <Link className="nav-name" to="/">Profile</Link> 
+              </li>
+              {checkLogedIn ? (
+                <div className="signup-login">
+                  <li className="nav-box">
+                    {" "}
+                    <Link className="nav-name" to="/signup">Sign Up</Link> 
+                  </li>
+                  <li className="nav-box">
+                    {" "}
+                    <Link className="nav-name" to="/login"> Login</Link> 
+                  </li>
+                </div>
+              ) : (
+                <li className="nav-box">
+                  {" "}
+                  <Link className="nav-name" to="/"></Link> Logout
+                </li>
+              )}
             </div>
           </ul>
         </div>
