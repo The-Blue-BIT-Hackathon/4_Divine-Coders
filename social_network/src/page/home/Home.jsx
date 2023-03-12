06c3f0df6dd119f62d29781e96d9db43f96434ff
Arnav Deshmukh <arnav.deshmukh20@pccoepune.org>
2023-03-12 13:46:27 +0530

    Data Fetching From FireStore Done

--- social_network/src/page/home/Home.jsx
+++ social_network/src/page/home/Home.jsx
@@ -1,6 +1,35 @@
-import React from 'react'
-
+import React, { useEffect,useState } from 'react'
+import { db } from '../../firebase/Firebase';
+import { getDocs,collection } from 'firebase/firestore';
 const Home = () => {
+
+
+  const [posts,getPosts]=useState([]);
+  const postCollectionRef=collection(db,"posts");
+  useEffect(()=>{
+getPostList()
+  },[])
+
+
+  const getPostList=async()=>{
+    try{
+
+      const data=await getDocs(postCollectionRef);
+      console.log(data)
+      const filteredData=data.docs.map((doc)=>({
+        ...doc.data(),
+        id:doc.id
+      }));
+      console.log(filteredData)
+      getPosts(filteredData);
+      console.log(posts)
+    }
+    catch(err)
+    {
+      console.error(err)
+    }
+
+  }
   return (
     <div>
         
